9aae36b18975849b0e572df23b5bd408a0a448fd
Zikri Azzuri <zikriazzuri5@gmail.com>
2026-09-22 05:43:13 +0700

    create function for login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,12 @@
 describe("Login SauceDemo", () => {
-  beforeEach(() => {
-    cy.visit("https://www.saucedemo.com//");
-  });
-
   it("berhasil login dengan akun valid", () => {
-    cy.get('[data-test="username"]').type("standard_user");
-    cy.get('[data-test="password"]').type("secret_sauce");
-    cy.get('[data-test="login-button"]').click();
+    cy.login("standard_user", "secret_sauce");
 
     cy.url().should("include", "/inventory.html");
   });
 
   it("gagal login dengan password yang salah", () => {
-    cy.get('[data-test="username"]').type("standard_user");
-    cy.get('[data-test="password"]').type("salah");
+    cy.login("standard_user", "salah");
     cy.get('[data-test="login-button"]').click();
 
     cy.get('[data-test="error"]').should("be.visible");
