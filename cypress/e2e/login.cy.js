a59d6814498a75e1b4bfc77ca98d6788552150ff
Zikri Azzuri <zikriazzuri5@gmail.com>
2026-09-23 06:07:03 +0700

    add problem_user image bug test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,27 @@ describe("Login SauceDemo", () => {
 
   it("gagal login dengan password yang salah", () => {
     cy.login("standard_user", "salah");
-    cy.get('[data-test="login-button"]').click();
 
     cy.get('[data-test="error"]').should("be.visible");
+  });
+
+  it("gagal login dengan akun yang di-lock", () => {
+    cy.login("locked_out_user", "secret_sauce");
+
+    cy.get('[data-test="error"]').should(
+      "have.text",
+      "Epic sadface: Sorry, this user has been locked out."
+    );
+  });
+
+  it("problem_user menampilkan gambar produk yang salah", () => {
+    cy.login("problem_user", "secret_sauce");
+
+    cy.get(".inventory_item_img").then(($images) => {
+      const sources = [...$images].map((image) => image.getAttribute("src"));
+      const uniqueSources = new Set(sources);
+
+      expect(uniqueSources.size).to.be.lessThan(sources.length);
+    });
   });
 });
